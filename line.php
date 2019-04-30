fdceac4effaa416f125d2bda17ff04a13afa8846
nattapong11200 <50147369+nattapong11200@users.noreply.github.com>
2019-05-01 00:31:17 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,37 +1,31 @@
  <?php
   
 
-function send_LINE($msg){
- $access_token = 'tYSCNQ/SHUpH8UuIgm60rl+Wv5P++2ai2/AbJYEYBXEllRHw29lifIaexiPHki7jQD3cjQNp1PDfob1joHuZVq79sdcOrHd8sPHTaklk+ASq99Tbv7UJuNp35Emtvhie/K9D0QrWX43efhPPduxdKgdB04t89/1O/w1cDnyilFU=
-'; 
 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-         $str = “Hello”;
-        //'text' => $text
-      ];
-
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://linebotics2.com';
-      $data = [
-
-        'Userid ' => 'U25b957160460cc73ee9487afc2dccad0',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-
-      echo $result . "\r\n"; 
+define(‘LINE_API’,”https://notify-api.line.me/api/notify");
+ 
+$token = “tYSCNQ/SHUpH8UuIgm60rl+Wv5P++2ai2/AbJYEYBXEllRHw29lifIaexiPHki7jQD3cjQNp1PDfob1joHuZVq79sdcOrHd8sPHTaklk+ASq99Tbv7UJuNp35Emtvhie/K9D0QrWX43efhPPduxdKgdB04t89/1O/w1cDnyilFU=
+”;
+$str = “Hello”; 
+ 
+$res = notify_message($str,$token);
+print_r($res);
+function notify_message($message,$token){
+ $queryData = array(‘message’ => $message);
+ $queryData = http_build_query($queryData,’’,’&’);
+ $headerOptions = array( 
+         ‘http’=>array(
+            ‘method’=>’POST’,
+            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
+                      .”Authorization: Bearer “.$token.”\r\n”
+                      .”Content-Length: “.strlen($queryData).”\r\n”,
+            ‘content’ => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
 }
 
 ?>
